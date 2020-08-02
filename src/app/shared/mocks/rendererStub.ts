3ca48393d1d13fa444acc7d220cb81442e686997
SmaLLAlien <vlad_belousov1990@hotmail.com>
2020-08-02 15:15:26 +0300

    Auto-generated commit

--- src/app/shared/mocks/rendererStub.ts
+++
@@ -1,6 +0,0 @@
-export class RendererStub {
-  setStyle = () => {
-  }
-  selectRootElement = () => {
-  }
-}
--- src/app/shared/mocks/tostrServiceStub.ts
+++
@@ -1,6 +0,0 @@
-export class TostrServiceStub {
-  show = () => {
-  }
-  error = () => {
-  }
-}
