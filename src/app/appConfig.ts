41607e8d4b1b235c58ea4fbcded63873bf3c00e3
SmaLLAlien <vlad_belousov1990@hotmail.com>
2020-08-02 18:23:57 +0300

    added appConfig, month model separate file

---
+++ src/app/appConfig.ts
@@ -0,0 +1,4 @@
+export enum URLS {
+  base = 'https://yalantis-react-school-api.yalantis.com/api/',
+  users = 'task0/users'
+}
---
+++ src/app/features/users/models/month.model.ts
@@ -0,0 +1,4 @@
+export class IMonth {
+  month: string;
+  usersQuantity: number;
+}
